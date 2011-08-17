faf1e48ff9756d017734e416463e24540ec44ff5
Ole Rixmann <rixmann.ole@googlemail.com>
2011-08-17 13:22:14 +0200

    logs für die ibrowse hinzugefügt

--- aggregator_connector.erl
+++ aggregator_connector.erl
@@ -189,33 +189,36 @@ handle_cast(go_get_messages, State = #state{subscription = Sub}) ->
 						       -2,
 						       <<"Network error, timeout">>),
 				   Sub),
+	    log("opening http connection failed on worker ~p for req_timedout", [get_id(State)]),	    	    
 	    CallbackOnConfig(State);
 	{error, {conn_failed, {error, timeout}}} -> 
 	    message_to_coordinator(create_prisma_error(list_to_binary(get_id(State)),
 						       -2,
 						       <<"Network error, connection failed -> timeout">>),
 				   Sub),
+	    log("opening http connection failed on worker ~p for timeout", [get_id(State)]),
 	    CallbackOnConfig(State);
-	{error, {conn_failed, {error, _}}} -> 
+	{error, {conn_failed, {error, _} = Error}} -> 
 	    message_to_coordinator(create_prisma_error(list_to_binary(get_id(State)),
 						       -2,
 						       <<"Network error, connection failed">>),
 				   Sub),
+	    log("opening http connection failed on worker ~p for Error~n~p", [get_id(State), Error]),
 	    CallbackOnConfig(State);
 	{error, _Reason} ->
 	    message_to_coordinator(create_prisma_error(list_to_binary(get_id(State)),
 						       -2,
 						       <<"Network error, undefinded">>),
 				   Sub),
-						%	    log("opening http connection failed on worker ~p for reason~n~p", [get_id(State), _Reason]),
+	    log("opening http connection failed on worker ~p for reason~n~p", [get_id(State), _Reason]),
 	    CallbackOnConfig(State);
 	{'EXIT', _} -> agr:callbacktimer(5, go_get_messages);
 	_Val -> 
-	    %log("opening http connection failed on worker ~p for Val~n~p", [get_id(State), Val]),
 	    message_to_coordinator(create_prisma_error(list_to_binary(get_id(State)),
 						       -2,
 						       <<"Network error, undefinded">>),
 				   Sub),
+	    log("opening http connection failed on worker ~p for Val~n~p", [get_id(State), _Val]),
 	    CallbackOnConfig(State)
     end;
 
